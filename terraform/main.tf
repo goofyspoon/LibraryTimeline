abbdd0a940371a2a39738463ec8644635b9ebd0e
Marissa Gustafson <marissag@iastate.edu>
2023-03-01 17:06:52 -0800

    Add inital terraform backup file

---
+++ terraform/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-current-state"
+    key            = "librarytimeline/terraform.tfstate"
+    region         = "us-east-2"
+
+    # dynamodb_table = "terraform-current-locks"
+    # encrypt        = true
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+}
+
+# Bucket to store terraform "up and running" state
+resource "aws_s3_bucket" "terraform_current_state" {
+  bucket = "terraform-current-state"
+  # The following should be used to ensure there is no accidental deletion
+  # For this project, enable deletion
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
+}
+
+# Enable versioning on the s3 bucket
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_current_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Enable Server Side Encryption by default for anything written to the s3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_current_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Block all public access to the s3 bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# Create DynamboDB Table to lock Terraform state, need to look into billing in AWS free tier
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "terraform-current-locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
