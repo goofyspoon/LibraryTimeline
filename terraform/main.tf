23f2ef7d2d3e3a4f79cbd0c8f14ddd0199fa4efb
Marissa Gustafson <marissag@iastate.edu>
2023-03-02 15:10:08 -0800

    add s3 key prefix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-current-state"
-    key            = "librarytimeline/terraform.tfstate"
+    key            = "global/s3/terraform.tfstate"
     region         = "us-east-2"
 
     # dynamodb_table = "terraform-current-locks"
