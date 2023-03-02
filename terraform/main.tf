0711be60f27243febc6b5ee04f6a560ac9e47527
Marissa Gustafson <marissag@iastate.edu>
2023-03-02 15:22:41 -0800

    Add dynamodb and refactor provider info

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,17 +4,9 @@ terraform {
     key            = "global/s3/terraform.tfstate"
     region         = "us-east-2"
 
-    # dynamodb_table = "terraform-current-locks"
-    # encrypt        = true
+    dynamodb_table = "terraform-current-locks"
+    encrypt        = true
   }
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-
-  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
@@ -24,7 +16,6 @@ provider "aws" {
 # Bucket to store terraform "up and running" state
 resource "aws_s3_bucket" "terraform_current_state" {
   bucket = "terraform-current-state"
-  region  = "us-east-2"
   # The following should be used to ensure there is no accidental deletion
   # For this project, enable deletion
   # lifecycle {
@@ -52,7 +43,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
 
 # Block all public access to the s3 bucket
 resource "aws_s3_bucket_public_access_block" "public_access" {
-  bucket                  = aws_s3_bucket.terraform_state.id
+  bucket                  = aws_s3_bucket.terraform_current_state.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
@@ -60,12 +51,12 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 }
 
 # Create DynamboDB Table to lock Terraform state, need to look into billing in AWS free tier
-# resource "aws_dynamodb_table" "terraform_locks" {
-#   name         = "terraform-current-locks"
-#   billing_mode = "PAY_PER_REQUEST"
-#   hash_key     = "LockID"
-#   attribute {
-#     name = "LockID"
-#     type = "S"
-#   }
-# }
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-current-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
